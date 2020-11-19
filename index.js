643feb369991efbda04468f1906c19ee0166036b
AssanSav <assane.savadogo81@gmail.com>
2020-11-18 22:10:24 -0500

    Refactored

--- index.js
+++ index.js
@@ -1,29 +1,18 @@
-const Joi = require("joi-browser")
-Joi.objectId = require("joi-objectid")(Joi)
-const mongoose = require("mongoose")
-const customers = require("./routes/customers")
-const genres = require("./routes/genres")
-const movies = require("./routes/movies")
-const rentals = require("./routes/rentals")
-const logger = require("./middleware/logger")
-const morgan = require("morgan")
-const helmet = require("helmet")
+const winston = require("winston")
 const express = require("express")
 const app = express()
 
-// Connect to MongoDB
-mongoose.connect("mongodb://localhost/vidly", { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Connected to mongoDB..."))
-  .catch((err) => console.log("Could not connect to mongoDB..."))
+require("./startup/config")()
+require("./startup/logging")(app)
+require("./startup/validation")()
+require("./startup/routes")(app)
+require("./startup/db")()
 
-app.use(logger)
-app.use(helmet())
-app.use(morgan("tiny"))
-app.use(express.json())
-app.use("/api/genres", genres)
-app.use("/api/customers", customers)
-app.use("/api/movies", movies)
-app.use("/api/rentals", rentals)
+
+// const p = Promise.reject(new Error("Something failed miserably!"))
+// p.then(() => console.log("Done!"))
+
+// throw new Error("Something failed during startup.") 
 
 const port = process.env.PORT || 3000 
-app.listen(port, () => console.log(`Listening on port ${port}...`))
+app.listen(port, () => winston.info(`Listening on port ${port}...`))

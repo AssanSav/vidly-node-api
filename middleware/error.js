75bcdfd077bc618793d26c21607095e545c88a78
AssanSav <assane.savadogo81@gmail.com>
2020-11-27 20:24:27 -0500

    Error middleware handler using winston

---
+++ middleware/error.js
@@ -0,0 +1,14 @@
+const winston = require("winston")
+
+const error = (err, req, res, next) => {
+  winston.error(err.message, err)
+  // error
+  // warn 
+  // info
+  // verbose
+  // debug
+  // silly
+  res.status(500).send("Something went wrong.")
+}
+
+module.exports = error

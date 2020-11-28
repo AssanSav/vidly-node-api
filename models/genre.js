94d7fdd6f49468efd6a38129782c87570f574114
AssanSav <assane.savadogo81@gmail.com>
2020-11-27 20:26:29 -0500

    Genre model

--- models/genre.js
+++ models/genre.js
@@ -10,14 +10,17 @@ const genreSchema = new mongoose.Schema({
   }
 })
 
+
 const Genre = mongoose.model("Genre", genreSchema)
+
 
 const validateGenre = (genre) => {
   const schema = {
-    name: Joi.string().min(3).required()
+    name: Joi.string().min(5).max(50).required()
   }
   return Joi.validate(genre, schema)
 };
+
 
 module.exports = {
   Genre, 
